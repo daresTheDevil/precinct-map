3d7bcf519343f29ed184877a377170807ee22c36
David Kay <dkay@mdek12.org>
2020-10-21 16:53:51 -0500

    messed with zoom levels

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app dark>
-    <v-app-bar :clipped-left="clipped" fixed app>
+    <v-app-bar fixed app>
       <v-toolbar-title v-text="title" />
       <v-spacer />
       <v-btn icon @click.stop="rightDrawer = !rightDrawer">
@@ -37,7 +37,7 @@ export default {
     return {
       right: true,
       rightDrawer: false,
-      title: 'Precint Map',
+      title: 'Precinct Map',
     }
   },
 }
--- pages/index.vue
+++ pages/index.vue
@@ -2,21 +2,26 @@
   <v-layout>
     <v-row>
       <v-col cols="12" class="py-0">
-        <no-ssr>
-          <l-map :zoom="7.5" :center="center" :options="leafletMapOptions">
-            <l-tile-layer
+        <client-only>
+          <l-map
+            ref="map"
+            :zoom="zoom"
+            :center="center"
+            :options="leafletMapOptions"
+          >
+            <!-- <l-tile-layer
               url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-            ></l-tile-layer>
+            ></l-tile-layer> -->
             <!-- <l-geo-json
               v-if="show"
               :geojson="geojson"
               :options="options"
               :options-style="styleFunction"
             /> -->
-            <l-geo-json v-if="show" :geojson="geojson" />
+            <l-geo-json ref="geoJson" v-if="show" :geojson="geojson" />
             <l-marker :lat-lng="[55.9464418, 8.1277591]"></l-marker>
           </l-map>
-        </no-ssr>
+        </client-only>
       </v-col>
     </v-row>
   </v-layout>
@@ -25,12 +30,16 @@
 <script>
 export default {
   components: {},
+  async fetch() {
+    this.geojson = await fetch(
+      'https://raw.githubusercontent.com/davidbkay/precinct-map/main/static/precincts.json'
+    ).then((res) => res.json())
+  },
   data: () => ({
     loading: false,
     show: true,
     enableTooltip: true,
-    zoom: 6,
-
+    zoom: 7.5,
     center: [32.612855, -89.902802],
     geojson: null,
     fillColor: '#e4ce7f',
@@ -39,16 +48,35 @@ export default {
       closePopupOnClick: false,
       doubleClickZoom: 'center',
       zoomDelta: 0.25,
+      zoomSnap: 0,
     },
   }),
-  async created() {
-    this.loading = true
-    const response = await fetch(
-      'https://raw.githubusercontent.com/davidbkay/precinct-map/main/static/precincts.json'
-    )
-    const data = await response.json()
-    this.geojson = data
-    this.loading = false
+  computed: {
+    initZoom() {
+      if (this.$vuetify.breakpoint.smAndDown) return 6
+      return 7.5
+    },
+  },
+  methods: {
+    zoomToState() {
+      console.log('map', this.$refs.map)
+      console.log('geojson', this.$refs.geoJson)
+      // this.$refs.map.fitBounds(this.$refs.geoJson.mapObject.getBounds())
+    },
+  },
+  updated() {
+    this.$nextTick(() => {
+      this.zoomToState()
+    })
+
+    // this.loading = true
+    // const response = await fetch(
+    //   'https://raw.githubusercontent.com/davidbkay/precinct-map/main/static/precincts.json'
+    // )
+    // const data = await response.json()
+    // this.geojson = data
+    // this.loading = false
+    // this.$refs.map.fitBounds(this.$refs.geojson.getBounds())
   },
 }
 </script>
