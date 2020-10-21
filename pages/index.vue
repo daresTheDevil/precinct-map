a7d7f4e06614b6f64865f01811ddc324810c3515
David Kay <dkay@mdek12.org>
2020-10-21 13:50:50 -0500

    simplified geojson file

--- pages/index.vue
+++ pages/index.vue
@@ -11,6 +11,13 @@
             <l-tile-layer
               url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
             ></l-tile-layer>
+            <!-- <l-geo-json
+              v-if="show"
+              :geojson="geojson"
+              :options="options"
+              :options-style="styleFunction"
+            /> -->
+            <l-geo-json v-if="show" :geojson="geojson" />
             <l-marker :lat-lng="[55.9464418, 8.1277591]"></l-marker>
           </l-map>
         </no-ssr>
@@ -23,11 +30,28 @@
 export default {
   components: {},
   data: () => ({
+    loading: false,
+    show: true,
+    enableTooltip: true,
+    zoom: 6,
+    center: [48, -1.219482],
+    geojson: null,
+    fillColor: '#e4ce7f',
+    url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
     leafletMapOptions: {
       closePopupOnClick: false,
       doubleClickZoom: 'center',
       zoomDelta: 0.25,
     },
   }),
+  async created() {
+    this.loading = true
+    const response = await fetch(
+      'https://raw.githubusercontent.com/davidbkay/precinct-map/main/static/precincts.json'
+    )
+    const data = await response.json()
+    this.geojson = data
+    this.loading = false
+  },
 }
 </script>
